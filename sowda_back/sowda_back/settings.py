bea1031a35e58d9d686d0ea5e6ded00842b216b2
Cyber Security 222103 <222103@int.local>
2024-05-09 15:38:42 +0500

    09.05-1

--- sowda_back/sowda_back/settings.py
+++ sowda_back/sowda_back/settings.py
@@ -68,8 +68,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://localhost:1651",
-    "http://127.0.0.1:1651",
+    "http://localhost:60058",
+    "http://127.0.0.1:60058",
     "http://10.0.2.2:8000",
 ]
 
